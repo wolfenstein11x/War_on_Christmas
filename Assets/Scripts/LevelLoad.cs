5defc20fb995171801fd5d1548d85533fb72e623
Charles <cwolfe7@asu.edu>
2021-05-19 21:32:57 -0700

    possible to lose now

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadLoseScreen()
+    {
+        SceneManager.LoadScene("Lose_Screen");
+    }
+}
---
+++ Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesDisplay : MonoBehaviour
+{
+    [SerializeField] int lives = 5;
+    Text livesText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        livesText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void LoseLife()
+    {
+        lives -= 1;
+        UpdateDisplay();
+
+        if (lives <= 0)
+        {
+            FindObjectOfType<LevelLoad>().LoadLoseScreen();
+        }
+    }
+
+    
+}
